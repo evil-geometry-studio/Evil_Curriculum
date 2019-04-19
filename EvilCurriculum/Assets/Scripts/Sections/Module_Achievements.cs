ffa5c3c2a75b4c3c0b787b85e2fc694681f65a32
DeveloperVix <vicomisterio@hotmail.com>
2019-04-19 15:59:52 -0500

    Other Sections added
    Projects added
    Team members added
    Achievements added
    Contact added
    
    The functionality works, missing information and other elements.

---
+++ EvilCurriculum/Assets/Scripts/Sections/Module_Achievements.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Module_Achievements : MonoBehaviour,IModule
+{
+    GameObject nextBtn;
+    GameObject backBtn;
+    public GameObject section;
+    private void Start() 
+    {
+        nextBtn = GameObject.Find("Canvas/Achievements_UI/BtnNext");  
+        backBtn = GameObject.Find("Canvas/Achievements_UI/BtnBack");
+        section.SetActive(false);   
+    }
+    public void DisableSection () 
+    {
+        Debug.Log ("Desactiva seccion");
+        section.GetComponent<Animator> ().SetTrigger ("change");
+        
+    }
+
+    public void EnableSection () 
+    {
+        Debug.Log ("Activa Seccion");
+        section.SetActive (true);
+    }
+
+    public void BtnNextText()
+    {
+        /*indexText = (indexText+1) % textUI.Length;
+        if(indexText == textUI.Length-1)
+        {
+            nextBtn.SetActive(false);
+        }
+        textStudio.text = textUI[indexText];*/
+    }
+
+    public void BtnBackText()
+    {
+        /*indexText = (indexText-1) % textUI.Length;
+        if(indexText == 0)
+        {
+            backBtn.SetActive(false);
+        }
+        textStudio.text = textUI[indexText];    */
+    }
+}
---
+++ EvilCurriculum/Assets/Scripts/Sections/Module_Contact.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Module_Contact : MonoBehaviour,IModule
+{
+    GameObject nextBtn;
+    GameObject backBtn;
+    public GameObject section;
+    private void Start() 
+    {
+        nextBtn = GameObject.Find("Canvas/Contact_UI/BtnNext");  
+        backBtn = GameObject.Find("Canvas/Contact_UI/BtnBack");
+        section.SetActive(false);   
+    }
+    public void DisableSection () 
+    {
+        Debug.Log ("Desactiva seccion");
+        section.GetComponent<Animator> ().SetTrigger ("change");
+        
+    }
+
+    public void EnableSection () 
+    {
+        Debug.Log ("Activa Seccion");
+        section.SetActive (true);
+    }
+}
