31cf771f55584fe37cb4e22623511edb45afe183
DeveloperVix <vicomisterio@hotmail.com>
2019-04-20 11:57:07 -0500

    Projects in progress
    The section for the projects in progress works.

--- EvilCurriculum/Assets/Scripts/Sections/Module_Projects.cs
+++ EvilCurriculum/Assets/Scripts/Sections/Module_Projects.cs
@@ -1,23 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Module_Projects : MonoBehaviour, IModule {
 
+    public GameObject[] backgroundsProjects;
+
+    [TextArea(3,10)]
+    public string[] textUI;
+    [TextArea(3,10)]
+    public string[] textTitleUI;
+    int indexText = 0;
+    public TextMeshProUGUI textProject;
+    public TextMeshProUGUI textTileProject;
     GameObject nextBtn;
     GameObject backBtn;
     public GameObject section;
     private void Start() 
     {
-        nextBtn = GameObject.Find("Canvas/WorkInProgress_UI/BtnNext");  
-        backBtn = GameObject.Find("Canvas/WorkInProgress_UI/BtnBack");
+        nextBtn = GameObject.Find("Canvas/WorksInProgress_UI/BtnNext");  
+        backBtn = GameObject.Find("Canvas/WorksInProgress_UI/BtnBack");
         section.SetActive(false);   
     }
     public void DisableSection () 
     {
         Debug.Log ("Desactiva seccion");
         section.GetComponent<Animator> ().SetTrigger ("change");
-        
     }
 
     public void EnableSection () 
@@ -28,21 +37,27 @@ public class Module_Projects : MonoBehaviour, IModule {
 
     public void BtnNextText()
     {
-        /*indexText = (indexText+1) % textUI.Length;
+        backgroundsProjects[indexText].SetActive(false);
+        indexText = (indexText+1) % textUI.Length;
         if(indexText == textUI.Length-1)
         {
             nextBtn.SetActive(false);
         }
-        textStudio.text = textUI[indexText];*/
+        textProject.text = textUI[indexText];
+        textTileProject.text = textTitleUI[indexText];
+        backgroundsProjects[indexText].SetActive(true);
     }
 
     public void BtnBackText()
     {
-        /*indexText = (indexText-1) % textUI.Length;
+        backgroundsProjects[indexText].SetActive(false);
+        indexText = (indexText-1) % textUI.Length;
         if(indexText == 0)
         {
             backBtn.SetActive(false);
         }
-        textStudio.text = textUI[indexText];    */
+        textProject.text = textUI[indexText];
+        textTileProject.text = textTitleUI[indexText];
+        backgroundsProjects[indexText].SetActive(true);
     }
 }
